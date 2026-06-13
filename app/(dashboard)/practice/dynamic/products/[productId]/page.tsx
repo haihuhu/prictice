9c59e15bd8e636d4fba9a7281f08bde20a822e2d
haihuhu <wxl0411@gmail.com>
2026-06-13 09:29:53 +0800

    refactor: restructure routing practice with dynamic [productId] page

--- app/(dashboard)/practice/dynamic/products/[productId]/page.tsx
+++ app/(dashboard)/practice/dynamic/products/[productId]/page.tsx
@@ -2,11 +2,7 @@ import { Button } from '@/components/ui/button';
 import { products } from '@/lib/data';
 import Link from 'next/link';
 
-const ProductIdPage = async ({
-  params,
-}: {
-  params: Promise<{ [key: string]: string | number | undefined }>;
-}) => {
+const ProductIdPage = async ({ params }: { params: Promise<{ productId: string }> }) => {
   const { productId } = await params;
   const product = products.find((item) => item.id === productId);
   return (
