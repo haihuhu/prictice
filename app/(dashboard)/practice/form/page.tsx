9da26816a142842aa2a6f4333b3dfedaad9b12fc
haihuhu <wxl0411@gmail.com>
2026-06-17 05:17:50 +0800

    add form validateform and handleSubmit

--- app/(dashboard)/practice/form/page.tsx
+++ app/(dashboard)/practice/form/page.tsx
@@ -1,160 +1,213 @@
 'use client';
 
+import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
-import React, { useState } from 'react';
+import { Textarea } from '@/components/ui/textarea';
+import { cn } from '@/lib/utils';
+import { useState } from 'react';
 
-interface FormType {
+interface FormValueType {
   username: string;
   age: number;
   gender: 'male' | 'female';
   email: string;
-  message: string;
+  description: string;
   subscribe: boolean;
 }
 
-interface FormErrorType {
+interface ErrorType {
   username: string;
   age: string;
   gender: string;
   email: string;
-  message: string;
-  subscribe: string;
+  description: string;
 }
 
 const FormPage = () => {
-  const [formValue, setFormValue] = useState<FormType>({
+  const [formValue, setFormValue] = useState<FormValueType>({
     username: '',
     age: 0,
     gender: 'male',
     email: '',
-    message: '',
+    description: '',
     subscribe: false,
   });
 
-  const [formErrors, setFormErrors] = useState<FormErrorType>({
+  const [errors, setErrors] = useState<ErrorType>({
     username: '',
     age: '',
     gender: '',
     email: '',
-    message: '',
-    subscribe: '',
+    description: '',
   });
 
-  const validateField = (fieldName: string, value: any): string => {
-    if (fieldName === 'username') {
-      if (!value) return 'username is required';
-      if (value.length < 2 || value.length > 20)
-        return 'username must be between 2 and 20 characters';
+  const validateForm = (formField: string, value: any): string => {
+    if (formField === 'username') {
+      if (value === '') return 'username is required';
+      if (value.length < 2 || value.length > 20) return 'username must be between 2 and 20';
       return '';
     }
-    if (fieldName === 'age') {
-      if (value < 18) return 'age must be greater than 18';
-      if (value > 100) return 'age must be less than 100';
+    if (formField === 'age') {
+      if (value < 18 || value > 100) return 'age must be between 18 and 100';
       return '';
     }
-    if (fieldName === 'email') {
-      if (!value) return 'email is required';
-      if (!value.includes('@')) return 'type correct email';
-      if (!value.includes('.')) return 'type correct email';
+    if (formField === 'gender') {
+      if (value !== 'male' && value !== 'female') return 'gender must be male or female';
       return '';
     }
-    if (fieldName === 'message') {
-      if (value.length < 10 || value.length > 500)
-        return 'message must be between 10 and 500 characters';
+    if (formField === 'email') {
+      if (!value.includes('@')) return 'email is not valid';
+      return '';
+    }
+    if (formField === 'description') {
+      if (value.length < 10 || value.length > 100) return 'description must be between 10 and 100';
       return '';
     }
     return '';
   };
 
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const newErrors = {
+      username: validateForm('username', formValue.username),
+      age: validateForm('age', formValue.age),
+      gender: validateForm('gender', formValue.gender),
+      email: validateForm('email', formValue.email),
+      description: validateForm('description', formValue.description),
+    };
+
+    setErrors(newErrors);
+
+    const hasError = Object.values(newErrors).some((e) => e !== '');
+
+    if (hasError) return;
+
+    console.log('formValue:', formValue);
+  };
+
   return (
     <>
-      <div className="flex flex-col w-full  md:mx-50 gap-5 px-2">
-        <div className=" gap-2">
-          <div className="flex gap-2">
-            <h1>UserName:</h1>
-            <Input
-              value={formValue.username}
-              onChange={(e) => {
-                setFormValue({ ...formValue, username: e.target.value });
-              }}
-              onBlur={() => {
-                const error = validateField('username', formValue.username);
-                setFormErrors({ ...formErrors, username: error });
-              }}
-            />
+      <form onSubmit={handleSubmit}>
+        <div className="w-full">
+          <div className="flex-col justify-between items-center w-full md:max-w-4xl mx-auto px-2 my-2">
+            <div className="flex">
+              <h2 className="w-24 text-lg font-bold">Username:</h2>
+              <Input
+                className={cn(errors.username && 'border-red-500')}
+                value={formValue.username}
+                onChange={(e) => {
+                  setFormValue({ ...formValue, username: e.target.value });
+                }}
+                onBlur={() => {
+                  const error = validateForm('username', formValue.username);
+                  setErrors({ ...errors, username: error });
+                }}
+              />
+            </div>
+            <p className="ml-24 text-red-500 text-sm">{errors.username}</p>
           </div>
-          {formErrors.username && <p className="ml-30 text-red-500">{formErrors.username}</p>}
-        </div>
-        <div className="gap-2">
-          <div className="flex gap-2">
-            <h1>Age:</h1>
-            <Input
-              value={formValue.age}
-              onChange={(e) => {
-                setFormValue({ ...formValue, age: Number(e.target.value) });
-              }}
-              onBlur={() => {
-                const error = validateField('age', formValue.age);
-                setFormErrors({ ...formErrors, age: error });
-              }}
-            />
+          <div className="flex-col justify-between items-center w-full md:max-w-4xl mx-auto px-2 my-2">
+            <div className="flex ">
+              <h2 className="w-24 text-lg font-bold">Age:</h2>
+              <Input
+                type="number"
+                className={cn(errors.age && 'border-red-500')}
+                value={formValue.age}
+                onChange={(e) => {
+                  setFormValue({ ...formValue, age: Number(e.target.value) });
+                }}
+                onBlur={() => {
+                  const error = validateForm('age', formValue.age);
+                  setErrors({ ...errors, age: error });
+                }}
+              />
+            </div>
+            <p className="ml-24 text-red-500 text-sm">{errors.age}</p>{' '}
           </div>
-          {formErrors.age && <p className="ml-30 text-red-500">{formErrors.age}</p>}
+          <div className="flex-col justify-between items-center w-full md:max-w-4xl mx-auto px-2 my-2">
+            <div className="flex ">
+              <h2 className="w-24 text-lg font-bold">Gender:</h2>
+              <select
+                title="gender"
+                className="rounded-lg border "
+                value={formValue.gender}
+                onChange={(e) => {
+                  setFormValue({ ...formValue, gender: e.target.value as 'male' | 'female' });
+                }}
+                onBlur={() => {
+                  const error = validateForm('gender', formValue.gender);
+                  setErrors({ ...errors, gender: error });
+                }}
+              >
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+              </select>
+            </div>
+            <p className="ml-24 text-red-500 text-sm">{errors.gender}</p>{' '}
+          </div>
+          {/* Email */}
+          <div className="flex-col justify-between items-center w-full md:max-w-4xl mx-auto px-2 my-2">
+            <div className="flex ">
+              <h2 className="w-24 text-lg font-bold">Email:</h2>
+              <Input
+                className={cn(errors.email && 'border-red-500')}
+                value={formValue.email}
+                onChange={(e) => {
+                  setFormValue({ ...formValue, email: e.target.value });
+                }}
+                onBlur={() => {
+                  const error = validateForm('email', formValue.email);
+                  setErrors({ ...errors, email: error });
+                }}
+              />
+            </div>
+
+            <p className="ml-24 text-red-500 text-sm">{errors.email}</p>
+          </div>
+
+          <div className="flex-col justify-between items-center w-full md:max-w-4xl mx-auto px-2 my-2">
+            <div className="flex ">
+              <h2 className="w-24 text-lg font-bold">Description:</h2>
+              <Textarea
+                className={cn(errors.description && 'border-red-500')}
+                value={formValue.description}
+                onChange={(e) => {
+                  setFormValue({ ...formValue, description: e.target.value });
+                }}
+                onBlur={() => {
+                  const error = validateForm('description', formValue.description);
+                  setErrors({ ...errors, description: error });
+                }}
+              />
+            </div>
+            <p className="ml-24 text-red-500 text-sm">{errors.description}</p>{' '}
+          </div>
+
+          <div className="flex-col justify-between items-center w-full md:max-w-4xl mx-auto px-2 my-2">
+            <div className="flex">
+              <h2 className="w-24 text-lg font-bold">Subscribe:</h2>
+              <Input
+                type="checkbox"
+                checked={formValue.subscribe}
+                onChange={(e) => {
+                  setFormValue({ ...formValue, subscribe: e.target.checked });
+                }}
+              />
+            </div>
+          </div>
+          <Button type="submit" className="flex w-full max-w-3xl mx-auto my-2 px-2">
+            Submit
+          </Button>
+          <div className="flex flex-col md:flex-row justify-between items-center w-full md:max-w-4xl gap-2 mx-auto px-2">
+            <div className="w-full flex-1 border-4 rounded-md break-all">
+              <pre className="whitespace-pre-wrap">{JSON.stringify(formValue, null, 2)}</pre>
+            </div>
+            <div className="w-full flex-1 border-4 rounded-md">
+              <pre className="whitespace-pre-wrap">{JSON.stringify(errors, null, 2)}</pre>
+            </div>
+          </div>
         </div>
-        <div className="flex gap-2">
-          <h1>Gender:</h1>
-          <select
-            title="Gender"
-            className="border rounded-md"
-            name="gender"
-            value={formValue.gender}
-            onChange={(e) => {
-              setFormValue({ ...formValue, gender: e.target.value as 'male' | 'female' });
-            }}
-          >
-            <option value="male">Male</option>
-            <option value="female">Female</option>
-          </select>
-        </div>
-        <div className="flex gap-2">
-          <h1>Email:</h1>
-          <Input
-            value={formValue.email}
-            onChange={(e) => {
-              setFormValue({ ...formValue, email: e.target.value });
-            }}
-          />
-        </div>
-        <div className="flex gap-2">
-          <h1>Message:</h1>
-          <Input
-            value={formValue.message}
-            onChange={(e) => {
-              setFormValue({ ...formValue, message: e.target.value });
-            }}
-          />
-        </div>
-        <div className="flex gap-2">
-          <h1>Subscribe:</h1>
-          <Input
-            type="checkbox"
-            checked={formValue.subscribe}
-            onChange={(e) => {
-              setFormValue({ ...formValue, subscribe: e.target.checked });
-            }}
-          />
-        </div>
-      </div>
-      <div className="flex justify-center items-center flex-col md:flex-row w-full md:max-w-4xl md:mx-auto gap-2 px-2 mt-2">
-        <div className="w-full md:flex-1 border-4 rounded-lg">
-          <h2 className="text-center text-lg font-bold">Form Value</h2>
-          <pre>{JSON.stringify(formValue, null, 2)}</pre>
-        </div>
-        <div className="w-full md:flex-1 border-4 rounded-lg">
-          <h2 className="text-center text-lg font-bold">Form Errors</h2>
-          <pre>{JSON.stringify(formErrors, null, 2)}</pre>
-        </div>
-      </div>
+      </form>
     </>
   );
 };
